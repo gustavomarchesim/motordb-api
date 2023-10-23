67683176412a1a4072270f98dda4de2c18e89e71
Gustavo Marchesim <105670464+gustavomarchesim@users.noreply.github.com>
2023-10-23 14:26:54 -0300

    Validando via trycatchs

--- pages/api/usuario.ts
+++ pages/api/usuario.ts
@@ -2,7 +2,13 @@ import { jwtValidator } from '@/middlewares/jwt_auth';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 const userEndpoint = (req: NextApiRequest, res: NextApiResponse) => {
-  return res.status(200).json('Usuário autenticado com sucesso!');
+  try {
+    return res.status(200).json('Usuário autenticado com sucesso!');
+  } catch (error) {
+    return res
+      .status(401)
+      .json({ mensagem: 'Erro durante a autenticação', error });
+  }
 };
 
 export default jwtValidator(userEndpoint);
